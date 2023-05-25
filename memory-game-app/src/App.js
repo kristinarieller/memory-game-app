f3885a060cabf1c2a530c107ce41f3358349a881
kristinarieller <kristinarieller@gmail.com>
2023-05-24 21:10:20 -0400

    added new game button

--- memory-game-app/src/App.js
+++ memory-game-app/src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import Cards from './Cards';
 
 function App() {
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
   return (
     <div className="App">
      <h1>Memory Game</h1>
      <Cards>
     
      </Cards>
+     <button onClick={refreshPage}>New Game</button>
     </div>
   );
 }
